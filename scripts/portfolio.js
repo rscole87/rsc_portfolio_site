a5ca002c5bf05773e5919cae58e76c267cc3c087
Rodrick Coleman <rodrickcoleman@MacBook-Pro-2.local>
2021-04-09 22:07:58 -0400

    optimize scripts for project thumbs and modal display

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,158 +1,98 @@
-let contactForm = document.getElementById("contact-form");
-let inquiries = [];
-let submissionModal = document.getElementById("submission-modal");
-let testBttn = document.getElementById("modal-test");
-let projectsDiv = document.getElementById("projects-div");
-let mobileMenuBttn = document.getElementById("menu-button");
-let mobileMenu = document.getElementById("mobile-nav-menu");
-let closeBttns = document.getElementsByClassName("close");
-let projectThumbs = document.getElementsByClassName("project-thumb");
+import projectList from "./projectList.js"
+let contactForm = document.getElementById("contact-form")
+let inquiries = []
+let submissionModal = document.getElementById("submission-modal")
+let projectsDiv = document.getElementById("projects-div")
+let projectsGrid = document.getElementById("projects-grid")
+let mobileMenuBttn = document.getElementById("menu-button")
+let mobileMenu = document.getElementById("mobile-nav-menu")
+let closeBttns = document.getElementsByClassName("close")
+let projectThumbs = document.getElementsByClassName("project-thumb")
+let projectTemplate = document.getElementById("project-template")
+let projectThumbTemplate = document.getElementById("project-thumb-template")
 
-console.log(firebase);
-
-
-contactForm.onsubmit = function(e){
-    e.preventDefault();
-    let fName = contactForm.f_name.value;
-    let lName = contactForm.l_name.value;
-    let email = contactForm.email.value;
-    let message = contactForm.message.value;
-    let userContact = new Contact(fName, lName, email, message);
-    inquiries.push(userContact);
-    inquiries.forEach(inquiry => inquiry.printContact());
-    contactForm.reset();
-    submissionModal.style.display = "block";
-    return userContact;
+contactForm.onsubmit = function (e) {
+  e.preventDefault()
+  let fName = contactForm.f_name.value
+  let lName = contactForm.l_name.value
+  let email = contactForm.email.value
+  let message = contactForm.message.value
+  let userContact = new Contact(fName, lName, email, message)
+  inquiries.push(userContact)
+  inquiries.forEach((inquiry) => inquiry.printContact())
+  contactForm.reset()
+  submissionModal.style.display = "block"
+  return userContact
 }
 
+const closeModal = (e) => {
+  if (e.target.parentElement.parentElement.classList.contains("modal")) {
+    toggleVisability(e.target.parentElement.parentElement)
+  }
+}
 
-const closeModal = (e) =>{
-    if (e.target.parentElement.parentElement.classList.contains("modal")) {
-        toggleVisability(e.target.parentElement.parentElement);
+const toggleVisability = (element) => {
+  if (element.style.display === "") {
+    element.style.display = "block"
+  } else {
+    element.style.display = ""
+  }
+}
+
+mobileMenuBttn.addEventListener("click", () => toggleVisability(mobileMenu))
+
+const displayProject = (projectId) => {
+  let selectedProject
+
+  projectList.forEach((project) => {
+    if (project.id == projectId) {
+      selectedProject = project
     }
+  })
+
+  let modalDiv = document.createElement("div")
+  modalDiv.className = "modal"
+
+  modalDiv.appendChild(printProject(selectedProject))
+  projectsDiv.appendChild(modalDiv)
+
+  modalDiv.style.display = "block"
 }
 
-const createCloseBttn = () =>{
-    let closeBttn = document.createElement("span");
-    closeBttn.innerHTML = "&times";
-    closeBttn.className = "close";
-    closeBttn.addEventListener("click", closeModal);
-    return closeBttn;
-}
- 
- 
-Array.from(projectThumbs).forEach(ele => {
-    ele.addEventListener("click", () =>{
-        console.log(ele.id)
-        displayProject(ele.id);
-    ;})
-}); 
-
-
-const toggleVisability = (element) =>{
-    if(element.style.display === ""){
-        element.style.display = "block";
-    } else {
-        element.style.display = "";
-    }
+const printProject = (project) => {
+  let projectClone = document.importNode(projectTemplate.content, true)
+  projectClone.querySelector("[project-name]").innerText = project.name
+  projectClone.querySelector("[project-description]").innerText = project.description
+  projectClone.querySelector("[project-img]").src = project.image
+  return projectClone
 }
 
-mobileMenuBttn.addEventListener("click", () => toggleVisability(mobileMenu));
-
-
-const displayProject = (ele) =>{
-    let selectedProject = projects[ele];
-
-    console.log("trying to display project");
-    let modalDiv = document.createElement("div");
-    modalDiv.className = "modal";
-
-    let modalContent = document.createElement("div");
-    modalContent.className = "modal-content"
-
-    let closeBttn = createCloseBttn();
-
-    let projectName = document.createElement("h4");
-    projectName.className = "project-name";
-    projectName.innerText = selectedProject.name;
-
-    let projectDescription = document.createElement("p");
-    projectDescription.className = "project-description";
-    projectDescription.innerText = selectedProject.description;
-
-    let imgDiv = document.createElement("div");
-
-    let projectImg = document.createElement("img");
-    projectImg.className = "project-img";
-    projectImg.src = selectedProject.image;
-
-    modalDiv.appendChild(modalContent);
-    modalContent.appendChild(closeBttn);
-    modalContent.appendChild(projectName);
-    modalContent.appendChild(projectDescription);
-    modalContent.appendChild(imgDiv);
-
-    imgDiv.appendChild(projectImg);
-
-    projectsDiv.appendChild(modalDiv);
-    modalDiv.style.display = "block";
+const createProjectThumb = (project) => {
+  let thumb = document.importNode(projectThumbTemplate.content, true)
+  thumb.querySelector("[project-id]").id = project.id
+  return thumb
 }
 
-
-let projects = {
-
-    project1: {
-        name: "Base Apparel",
-        description: "Landing page for a new clothing retailer.",
-        image: "./images/projects/base_apparel.png",
-        // gitHub: ,
-        // demo: ,
-    },
-
-    project2: {
-        name: "Intro Component",
-        description: "Registration form for online learning platform.",
-        image: "./images/projects/intro_component.png",
-        // gitHub: ,
-        // demo: ,
-    },
-
-    project3: {
-        name: "Bookmark Manager",
-        description: "Website for a bookmark management platform.",
-        image: "./images/projects/bookmark_manager.png",
-        // gitHub: ,
-        // demo: ,
-    },
-
-    project4: {
-        name: "Smash Tourney Bracket",
-        description: "Tournament registration form and bracket tracker.",
-        image: "./images/projects/smash_bracket.png",
-        // gitHub: ,
-        // demo: ,
-    },
-
-    project5: {
-        name: "To-Do List",
-        description: "Task tracking application.",
-        image: "./images/projects/to_do_list.png",
-        // gitHub: ,
-        // demo: ,
-    },
-
-    project6: {
-        name: "Tip Calculator",
-        description: "Tip calculator for individuals and groups.",
-        image: "./images/projects/tip_calc.png",
-        // gitHub: ,
-        // demo: ,
-    }
+const populateAllProjects = () => {
+  projectList.forEach((project) => {
+    projectsGrid.appendChild(createProjectThumb(project))
+  })
 }
 
+populateAllProjects()
+
+Array.from(projectThumbs).forEach((ele) => {
+  ele.addEventListener("click", () => {
+    displayProject(ele.id)
+
+    Array.from(closeBttns).forEach((bttn) => {
+      bttn.addEventListener("click", closeModal)
+    })
+  })
+})
 
 window.onclick = function (event) {
-    if (event.target.classList.contains("modal")){
-        toggleVisability(event.target);
-    }
+  if (event.target.classList.contains("modal")) {
+    toggleVisability(event.target)
+  }
 }
--- scripts/projectList.js
+++ scripts/projectList.js
@@ -1,53 +1,57 @@
+const projectList = [
+  {
+    id: "project1",
+    name: "Base Apparel",
+    description: "Landing page for a new clothing retailer.",
+    image: "../images/projects/base_apparel.png",
+    // gitHub: ,
+    // demo: ,
+  },
 
-module.exports(projectList)= {
+  {
+    id: "project2",
+    name: "Intro Component",
+    description: "Registration form for online learning platform.",
+    image: "../images/projects/intro_component.png",
+    // gitHub: ,
+    // demo: ,
+  },
 
-    project1: {
-        name: "Base Apparel",
-        desription: "Landing page for a new clothing retailer.",
-        image: "../images/projects/base_apparel.png",
-        // gitHub: ,
-        // demo: ,
-    },
+  {
+    id: "project3",
+    name: "Bookmark Manager",
+    description: "Website for a bookmark management platform.",
+    image: "../images/projects/bookmark_manager.png",
+    // gitHub: ,
+    // demo: ,
+  },
 
-    project2: {
-        name: "Intro Component",
-        desription: "Registration form for online learning platform.",
-        image: "../images/projects/intro_component.png",
-        // gitHub: ,
-        // demo: ,
-    },
+  {
+    id: "project4",
+    name: "Smash Tourney Bracket",
+    description: "Tournament registration form and bracket tracker.",
+    image: "../images/projects/smash_bracket.png",
+    // gitHub: ,
+    // demo: ,
+  },
 
-    project3: {
-        name: "Bookmark Manager",
-        desription: "Website for a bookmark management platform.",
-        image: "../images/projects/bookmark_manager.png",
-        // gitHub: ,
-        // demo: ,
-    },
+  {
+    id: "project5",
+    name: "To-Do List",
+    description: "Task tracking application.",
+    image: "../images/projects/to_do_list.png",
+    // gitHub: ,
+    // demo: ,
+  },
 
-    project4: {
-        name: "Smash Tourney Bracket",
-        desription: "Tournament registration form and bracket tracker.",
-        image: "../images/projects/smash_bracket.png",
-        // gitHub: ,
-        // demo: ,
-    },
+  {
+    id: "project6",
+    name: "Tip Calculator",
+    description: "Tip calculator for individuals and groups.",
+    image: "../images/projects/tip_calc.png",
+    // gitHub: ,
+    // demo: ,
+  },
+]
 
-    project5: {
-        name: "To-Do List",
-        desription: "Task tracking application.",
-        image: "../images/projects/to_do_list.png",
-        // gitHub: ,
-        // demo: ,
-    },
-
-    project6: {
-        name: "Tip Calculator",
-        desription: "Tip calculator for individuals and groups.",
-        image: "../images/projects/tip_calc.png",
-        // gitHub: ,
-        // demo: ,
-    }
-}
-
-export default(projectList);
+export default projectList
