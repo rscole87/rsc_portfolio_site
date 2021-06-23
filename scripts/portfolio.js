ebda6e2e4218a4ba850e304ae22ced405590befb
Rodrick Coleman <rscole87@gmail.com>
2021-06-22 22:10:21 -0400

    format dynamic project object data for modal

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -2,7 +2,11 @@ import projectList from "./projectList.js"
 let projectsGrid = document.getElementById("projects-grid")
 let projectTemplate = document.getElementById("project-template")
 let projectThumbTemplate = document.getElementById("project-thumb-template")
-
+let activeProjectVideo = document.getElementById("active-project-video")
+let activeProjectName = document.getElementById("active-project-name")
+let activeProjectDescription = document.getElementById("active-project-description")
+let activeProjectDemo = document.getElementById("active-project-demo")
+let activeProjectGithub = document.getElementById("active-project-github")
 
 const createProjectThumb = (project) => {
   let thumb = document.importNode(projectThumbTemplate.content, true)
@@ -19,13 +23,19 @@ const populateAllProjects = () => {
 populateAllProjects();
 
 const projectThumbLinks = Array.from(document.getElementsByClassName("project-thumb"))
+console.log(projectThumbLinks)
 
-projectThumbLinks.forEach(function (img) {
-  img.addEventListener("click", function () {
-    focusProject(img)
+projectThumbLinks.forEach(function (project) {
+  project.addEventListener("click", function () {
+    let selectedProject = projectList.filter(ele => ele.id === project.id)[0]
+    focusProject(selectedProject)
   })
 })
 
-const focusProject = function (img) {
-  activeImage.src = img.src
+const focusProject = function (selectedProject) {
+  activeProjectName.innerText = selectedProject.name;
+  activeProjectVideo.src = selectedProject.video;
+  activeProjectDescription.innerText = selectedProject.description;
+  activeProjectDemo.href = selectedProject.demo;
+  activeProjectGithub.href = selectedProject.github;
 }
